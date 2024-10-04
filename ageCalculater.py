7454c170118fd8aa8d85ed2d84a3f9857cfa35ab
simonstreuli <112710583+simonstreuli@users.noreply.github.com>
2024-10-04 10:00:41 +0200

    update

--- ageCalculater.py
+++ ageCalculater.py
@@ -2,12 +2,14 @@ from datetime import datetime
 from dateutil.relativedelta import relativedelta
 import sys
 
+
 def validate_date(date_text):
     try:
         return datetime.strptime(date_text, '%d.%m.%Y')
     except ValueError:
         print("Fehler: Falsches Datumformat. Bitte in der Form dd.mm.yyyy eingeben.")
         sys.exit(1)
+
 
 def calculate_age(birth_date):
     today = datetime.today()
@@ -21,6 +23,7 @@ def calculate_age(birth_date):
 
     return years, months, days, total_days
 
+
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Fehler: Bitte das Geburtsdatum in der Form dd.mm.yyyy eingeben.")
@@ -33,4 +36,5 @@ if __name__ == "__main__":
     years, months, days, total_days = calculate_age(birth_date)
 
     # Ausgabe
-    print(f"Das Alter ist {years} Jahre {months} Monate und {days} Tage, das sind {total_days} Tage insgesamt.")
+    print(
+        f"Das Alter ist {years} Jahre {months} Monate und {days} Tage, das sind {total_days} Tage")
